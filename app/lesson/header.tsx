6f58724feb6bccddb427696fec385752d8eab9ff
trungtan <trungtan14@gmail.com>
2024-03-22 11:02:46 +0700

    done card

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -13,22 +13,22 @@ const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
   const { open } = useExitModal();
 
   return (
-    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full ">
+    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <X
-        className="text-slate-500 hover:opacity-75 transition cursor-pointer"
         onClick={open}
+        className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
       <Progress value={percentage} />
       <div className="text-rose-500 flex items-center font-bold">
         <Image
           src="/heart.svg"
-          className="mr-2"
-          alt="heart"
           height={28}
           width={28}
+          alt="Heart"
+          className="mr-2"
         />
         {hasActiveSubscription ? (
-          <InfinityIcon className="h-6 w-6 stroke-[3]" />
+          <InfinityIcon className="h-6 w-6 stroke-[3] shrink-0" />
         ) : (
           hearts
         )}
--- app/lesson/layout.tsx
+++ app/lesson/layout.tsx
@@ -1,9 +1,15 @@
-const LessonLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
+type Props = {
+  children: React.ReactNode;
+};
+
+const LessonLayout = ({ children }: Props) => {
+  return ( 
     <div className="flex flex-col h-full">
-      <div className="flex flex-col h-full w-full">{children}</div>
+      <div className="flex flex-col h-full w-full">
+        {children}
+      </div>
     </div>
   );
 };
-
+ 
 export default LessonLayout;
